855b580bce2fcfce88fc09178674c6b2c0a4763c
Robin Dittrich <robinodittrich@gmail.com>
2021-12-13 20:46:47 -0800

    Add actors to Studio associations

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -3,8 +3,6 @@ require 'rails_helper'
 RSpec.describe Movie, type: :model do
   describe 'relationships' do
     it { should belong_to(:studio) }
+    it { should have_many(:actors).through(:studio) }
   end
 end
-# before(:each) do
-#   @movie_1 = Movie.create!(title: "Star Wars", create_year: "2001", genre: "Sci-fi")
-# end
