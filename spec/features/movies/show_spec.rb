a814c38e893de7cb555b00a794c778c677d4c61c
Robin Dittrich <robinodittrich@gmail.com>
2021-12-13 20:45:40 -0800

    Create tests for show page

---
+++ spec/features/movies/show_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe 'Movies show page' do
+  before(:each) do
+    @studio_1 = Studio.create!(name: "Universal Studios", location: "Hollywood")
+    @movie_1 = @studio_1.movies.create!(title: "Star Wars", creation_year: "2001", genre: "Sci-fi")
+    @movie_2 = @studio_1.movies.create!(title: "Gremlins", creation_year: "1995", genre: "Fantasy")
+    @actor_1 = @studio_1.actors.create!(name: "Harrison Ford", age: 78)
+    @actor_2 = @studio_1.actors.create!(name: "Natalie Portman", age: 40)
+    @actor_2 = @studio_1.actors.create!(name: "Adam Driver", age: 35)
+  end
+
+  it 'shows the title, creation year and genre of the movie' do
+    visit "/movies/#{@movie_1.id}"
+
+    expect(page).to have_content(@movie_1.title)
+    expect(page).to have_content(@movie_1.creation_year)
+    expect(page).to have_content(@movie_1.genre)
+  end
+end
